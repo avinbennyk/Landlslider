4d9251c57cbdc09c830e1c21c84cfbe973c31357
Avin Benny K <avinbenny8@gmail.com>
2025-03-13 16:11:36 +0530

    feat: added localhost to cors

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import predictions, admin
 
 app = FastAPI(
     title="GeoPredict Backend",
     description="Real-time landslide prediction backend.",
     version="1.0.0"
+)
+
+# Set up CORS middleware
+origins = [
+    "http://localhost:3001",  # Adjust the port if your frontend runs on a different one
+    "http://127.0.0.1:3000"   # You can include more origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include routers
